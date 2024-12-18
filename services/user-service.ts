dd0134e5884bd013cc0de3a8df7d87b4fdc590b9
Ananda Rizki Amelia <spicaonge@gmail.com>
2024-12-18 09:35:08 +0700

    feat: :sparkles: `Added Bootstrap CSS and JavaScript to Next.js project`

---
+++ services/user-service.ts
@@ -0,0 +1,15 @@
+import callAPI from "@/config/api";
+
+const ROOT_API = process.env.NEXT_PUBLIC_API;
+const API_VERSION = 'api/v1';
+
+export async function userService(){
+    const url = `${ROOT_API}/${API_VERSION}/users`;
+
+    return callAPI({
+        url,
+        method: 'GET',
+        isToken: true,
+
+    });
+}
