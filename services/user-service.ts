a5f2baf8a433648e6754e0a4b5abe34dc34e7bc8
Ananda Rizki Amelia <spicaonge@gmail.com>
2025-01-13 11:30:54 +0700

    feat: :sparkles: `feat: updated callAPI function and added user service with data fetching`

--- services/user-service.ts
+++ services/user-service.ts
@@ -4,7 +4,7 @@ const ROOT_API = process.env.NEXT_PUBLIC_API;
 const API_VERSION = 'api/v1';
 
 export async function userService(){
-    const url = `${ROOT_API}/${API_VERSION}/users`;
+    const url = `${ROOT_API}/${API_VERSION}/user`;
 
     return callAPI({
         url,
